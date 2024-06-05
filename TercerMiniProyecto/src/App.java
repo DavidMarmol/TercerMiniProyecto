380bedc6cee0eac1b48ed7befd64790f4082b9d6
juanjounivalle18 <juan.jose.millan@correounivalle.edu.com>
2024-06-04 22:18:18 -0500

    se añadio la carpeta vista para completar la parte visual

--- TercerMiniProyecto/src/App.java
+++ TercerMiniProyecto/src/App.java
@@ -1,5 +1,12 @@
+import modelo.Juego;
+import vista.VistaJuegoConsola;
+import vista.VistaJuegoGUI;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Juego modelo = new Juego();
+        VistaJuegoConsola vista = new VistaJuegoConsola();
+        //VistaJuegoGUI vista = new VistaJuegoGUI();
+
     }
 }
